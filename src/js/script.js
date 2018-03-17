6e8736ff0f24cd1e61d04171df9b4c9b4dabe125
imac <imac@MacBook-Air-imac.local>
2018-03-17 16:48:17 +0200

    velox

---
+++ src/js/script.js
@@ -0,0 +1,115 @@
+const toggleMenu = $('#toggle-menu');
+const navMenu = $('#nav-menu');
+
+toggleMenu.click(function(){
+    const $this= $(this);
+    let h;
+    if(navMenu.is(':hidden')) {
+        h = navMenu.show().height();
+            navMenu.css('height', 0);
+
+            navMenu.animate({'height': h}, 500, function(){
+                $this.addClass('active');
+            });
+    } else {
+        navMenu.animate({'height': 0}, 500, function(){
+            $this.removeClass('active');
+            navMenu.removeAttr('style').hide();
+        });
+    }
+});
+
+function initMobile() {
+
+    $('body').addClass('is-mobile').removeClass('is-desktop');
+    console.log("is-mobile");
+}
+
+
+function initDesktop() {
+    $('body').addClass('is-desktop').removeClass('is-mobile');
+    toggleMenu.removeClass('active');
+    navMenu.removeAttr('style');
+    console.log("is-desktop");
+ }
+
+
+
+
+
+ssm.addState({
+    id: 'tablet',
+    query: '(max-width: 768px)',
+    onEnter: function(){
+        initMobile();
+    }
+});
+
+ssm.addState({
+    id: 'desktop',
+    query: '(min-width: 768px)',
+    onEnter: function(){
+        initDesktop();
+    }
+});
+
+
+
+$("#slider").slick({
+
+        centerMode: true,
+        centerPadding: '150px',
+        slidesToShow: 2,
+    dots:true,
+    dotsClass: 'mydots'
+});
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+});
+
+
+
+$(function() {
+
+    var newSelection = "";
+
+    $("#flavor-nav a").click(function(){
+
+        $("#all-flavors").fadeTo(200, 0.10);
+
+        $("#flavor-nav a").removeClass("current");
+        $(this).addClass("current");
+
+        newSelection = $(this).attr("rel");
+
+        $(".flavor").not("."+newSelection).slideUp();
+        $("."+newSelection).slideDown();
+
+        $("#all-flavors").fadeTo(600, 1);
+
+    });
+
+});
